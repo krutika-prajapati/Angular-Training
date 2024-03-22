9452af263e458cfdbb7e849e0190a6f6a18d43a8
krutikaprajapati <krutikapra92@gmail.com>
2024-03-22 09:52:28 +0530

    format the code

--- Typescript/personInfo.js
+++ Typescript/personInfo.js
@@ -1,22 +1,26 @@
 var personInfo = {
-    firstname: 'Krutika',
-    lastname: 'Prajapati',
+    firstname: "Krutika",
+    lastname: "Prajapati",
     age: 22,
     address: {
-        city: 'Ahmedabad',
-        street: 'Vastral',
-        pincode: 382418
+        city: "Ahmedabad",
+        street: "Vastral",
+        pincode: 382418,
     },
     contact: {
-        phoneNumber: ['8989898999', '9999999999']
+        phoneNumber: ["8989898999", "9999999999"],
     },
     getFullName: function () {
         return "Fullname:".concat(this.firstname, " ").concat(this.lastname);
     },
     getFullAddress: function () {
         return "Address:".concat(this.address.street, ", ").concat(this.address.city, ", ").concat(this.address.pincode);
-    }
+    },
 };
 console.log(personInfo.getFullName());
 console.log(personInfo.getFullAddress());
 console.log(personInfo.contact);
+//Output:
+// Fullname:Krutika Prajapati
+// Address:Vastral, Ahmedabad, 382418
+// { phoneNumber: [ '8989898999', '9999999999' ] }
